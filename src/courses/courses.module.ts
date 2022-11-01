26ce2a70f5ec063620c8ed27e6575e5424a0fe9d
vandrehribeiro <vandreh.ribeiro@gethash.com.br>
2022-10-31 21:50:25 -0300

    first commit

---
+++ src/courses/courses.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { CoursesController } from './courses.controller';
+import { CoursesService } from './courses.service';
+import { DatabaseModule } from '../database/database.module';
+import { coursesProviders } from './courses.providers';
+
+@Module({
+    imports: [DatabaseModule],
+    controllers: [CoursesController],
+    providers: [CoursesService, ...coursesProviders],
+  })
+  export class CoursesModule {}
+  
